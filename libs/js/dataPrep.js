6a446e1420d40dfc238c809bda1f3747d1094139
DaGuT <mangustalex11@gmail.com>
2018-01-27 15:33:22 +0700

    added tree view, but have not yet finished it

---
+++ libs/js/dataPrep.js
@@ -0,0 +1,155 @@
+//Также можно добавить парсинг строки адреса и её изменение, чтобы можно было передавать друзьям, однако из-за возможности удаления карточек считаю эту затею бессмысленной и делаю текущий простой вариант
+
+$(document).ready(function () {
+    var loader = document.createElement("DIV");
+    loader.id = "loadingInProgress";
+    loader.innerHTML = svgLoading;
+    $("#deck").append(loader);
+});
+
+//Первым делом парсим JSON и делаем работу с ним
+function reloadJSON() { //Функция для простоты повторного вызова
+    $.get('http://93.91.165.233:8081/frontend_data/catalog.json', loadedJSON);
+}
+reloadJSON();
+
+//Будем хранить загруженный файл, т.к. нам с ним ещё много и долго работать :)
+var ourJSON;
+var curSort = ""; //Эта переменная нужна, чтобы можно было пересортировывать по убыванию или возрастанию без необходимости лишний раз перезапоминать
+var isDescending = true; //А эта переменная нужна для текущего порядка сортировки, чтобы его поменять
+var howMany = $("input[name=perPage]:checked")[0].value; //Число отображаемых карточек на странице. Честно, проще засунуть в эту одну переменную, чем постоянно вызывать селектор. Да и бесит этот селектор длинный писать :) Мы ж за оптимизацию рабочего процесса?
+var curPage;
+
+function loadedJSON(data) {
+    //Просто запомним содержимое этого JSONa
+    ourJSON = data.filter(function (item) {
+        if (localStorage.getItem(item.image)) {
+            return false;
+        }
+        return true;
+    });
+    //И удалим загрузочную штуку
+    resort(ourJSON, curSort, !isDescending); //После загрузки пересортируем заного
+
+    initView();
+
+}
+
+function initView() {
+    //В зависимости от режима инициализируем старт
+    if ($('input[name=show_like]:checked')[0].id === "grid") {
+        initDeck();
+        //И добавим странички
+        addPaginator();
+    } else {
+        initTree();
+        //И тут добавить функцию включения дерева
+        tree();
+    }
+}
+
+function initDeck() {
+
+    $("#list").html('<div class="col-table" id="deck">' +
+        '<!--Сюда будут вставлять все карточки -->' +
+        '</div>' +
+        '<div class="container offset-1 offset-sm-2 offset-md-4" id="paginator">' +
+        '<!--А здесь будут странички -->' +
+        '</div>');
+    $('#cardsSetting').show();
+}
+
+function initTree() {
+    $("#list").html('<div class="treeBrowser"><div id="jstree"></div></div>');
+    $('#cardsSetting').hide();
+}
+
+//изменяем вид структуры
+$('input[name=show_like]').change(function () {
+    initView();
+});
+
+//Сбрасываем удаленные картинки
+function resetCards() {
+    hideAll();
+    localStorage.clear();
+    reloadJSON();
+}
+
+//--------ВСё про сортировку
+//Сортируем :)
+//Тут ивент на клик. если был бы change, то радиобаттон бы не выстреливал. А задание именно на радио
+$("input[name=sortBy]").click(function () {
+    var newSort = $('input[name=sortBy]:checked')[0].id; //Запомним, какой новый метод сортировки
+    if (curSort !== newSort) { //Если у нас раньше была другая сортировка, то сортируем в новой в порядке убывания
+        resort(ourJSON, newSort); //Просто пересортируем в порядка возрастания
+        isDescending = true; //Отсортировано по убыванию
+    } else { //Если у нас уже была та же самая сортировка, то мы просто меняем на противоположную
+        if (isDescending) {
+            resort(ourJSON, newSort, true);
+        } else {
+            resort(ourJSON, newSort, false);
+        }
+        isDescending = !isDescending; //И меняем порядок сортировки в памяти
+    }
+    curSort = newSort; //Перезапоминаем
+    if ($('input[name=show_like]:checked')[0].id === "grid") {
+        hideAll();
+        addPaginator();
+    } else {
+        //Сброс и перестройка дерева
+        $.jstree.destroy();
+        tree();
+    }
+
+});
+
+
+//А тут сама сортировка
+function resort(array, type, isReversed) { //Если ничего не передаём в isReversed, то он равен undefined, что при сравнениях переводится в 0
+    switch (type) {
+        case "name":
+            ourJSON.sort(compareName);
+            if (isReversed) ourJSON.reverse();
+            break;
+        case "time":
+            ourJSON.sort(compareTime);
+            if (isReversed) ourJSON.reverse();
+            break;
+        case "category":
+            ourJSON.sort(compareCategory);
+            if (isReversed) ourJSON.reverse();
+            break;
+        case "size":
+            ourJSON.sort(compareSize);
+            if (isReversed) ourJSON.reverse();
+            break;
+        default:
+            break;
+    }
+}
+
+//---------Функции для сравнения, чтобы подать в arr.sort(). Подаём именно так, чтобы по-умолчанию был порядок убывания
+function compareTime(a, b) {
+    return b.timestamp - a.timestamp;
+}
+
+function compareCategory(a, b) {
+    return b.category > a.category ? 1 : -1;
+}
+
+function compareName(a, b) {
+    //Сперва выделим имя
+    function getName(filename) {
+        var pos = filename.lastIndexOf("/");
+        if (pos > -1) {
+            filename = filename.slice(pos + 1);
+        }
+        return filename;
+    }
+    return getName(b.image) > getName(a.image) ? 1 : -1;
+}
+
+function compareSize(a, b) {
+    return b.filesize - a.filesize;
+}
---
+++ libs/js/tree.js
@@ -0,0 +1,41 @@
+function prepDataForTree(isLightbox) {
+    //Можно, конечно, делать через ul,li, но зачем? Так проще :)
+    var result = []; //Массив, который подходит под jstree
+    var temp = {}; //Объект, который будем помещать в массив
+    var categories = {};
+    ourJSON.forEach(function (img) {
+        temp.id = img.image;
+        temp.parent = img.category;
+        categories[temp.parent]=true; //Более быстрого способа отдельно запомнить категории я не знаю пока :)
+        temp.text = "";
+        temp.icon = "http://93.91.165.233:8081/frontend_data/" + img.image;
+        if (isLightbox) { //Мы предоставим возможность выбора вида "как браузер" или "просто список с лайтбоксом"
+            temp.a_attr = {
+                href: "http://93.91.165.233:8081/frontend_data/" + img.image,
+                class: 'lightbox'
+            };
+        }
+        result.push(temp);
+        temp = {};
+    });
+    for (category in categories) {
+        result.push({
+            "id":category,
+            "text":category,
+            "parent":"#"
+        });
+    };
+    return result;
+}
+
+function tree() {
+    $('#jstree').jstree({
+        'core': {
+            'data': prepDataForTree(true)
+        }
+    });
+
+    $('#jstree').bind('ready.jstree', function () {
+        lightBox();
+    })
+}
